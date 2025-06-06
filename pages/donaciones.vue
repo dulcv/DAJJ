96b4e764f0ad8aa7491b8c0df7195c8959dcec24
Jessi <jessicagalindogarcia88@gmail.com>
2025-06-05 20:13:58 -0600

    Comit nuevo

--- pages/donaciones.vue
+++ pages/donaciones.vue
@@ -1,135 +1,150 @@
-<template>
+<!--<template>
+  <div id="app">
+    <header>
+      <h1>{{ titulo }}</h1>
+    </header>
 
-    <div id="app">
-        <header>
-            <h1>{{ titulo }}</h1>
-        </header>
+    <main>
+      <h2>Preguntas Frecuentes</h2>
+      <section class="faq">
+        <details v-for="(item, i) in preguntas" :key="i">
+          <summary>{{ item.pregunta }}</summary>
+          <p>{{ item.respuesta }}</p>
+        </details>
+      </section>
 
-        <main>
-            <h2>Preguntas Frecuentes</h2>
-            <section class="faq">
-                <details v-for="(item, i) in preguntas" :key="i">
-                    <summary>{{ item.pregunta }}</summary>
-                    <p>{{ item.respuesta }}</p>
-                </details>
-            </section>
+      <h2 id="donacion">¡Haz tu donación!</h2>
 
-            <h2 id="donacion">¡Haz tu donación!</h2>
+      <h3>1.Datos personales:</h3>
+      <form @submit.prevent="enviarDonacion">
+        <label>Nombre completo</label>
+        <input
+          type="text"
+          v-model="nombre"
+          placeholder="Ingrese su nombre completo"
+        />
 
-            <h3>1.Datos personales:</h3>
-            <form @submit.prevent="enviarDonacion">
-                <label>Nombre completo</label>
-                <input type="text" v-model="nombre" placeholder="Ingrese su nombre completo">
+        <label>Correo electrónico</label>
+        <input
+          type="email"
+          v-model="correo"
+          placeholder="Ingrese su correo electronico"
+        />
 
-                <label>Correo electrónico</label>
-                <input type="email" v-model="correo" placeholder="Ingrese su correo electronico">
+        <label for="telefono">Teléfono</label>
+        <input
+          type="tel"
+          id="telefono"
+          name="telefono"
+          placeholder="Ingrese su telefono"
+          v-model="telefono"
+        />
 
-                <label for="telefono">Teléfono</label>
-                <input type="tel" id="telefono" name="telefono" placeholder="Ingrese su telefono" v-model="telefono">
+        <h3>2.Seleccione un monto:</h3>
+        <div class="montos">
+          <label v-for="(monto, i) in montos" :key="i">
+            <input type="radio" :value="monto" v-model="montoSeleccionado" />
+            {{ monto }}
+          </label>
+          <div class="monto-personalizado">
+            <span>Otro:</span>
+            <input
+              type="number"
+              v-model="otro"
+              placeholder="Ingresa el monto"
+            />
+          </div>
+        </div>
 
-                <h3>2.Seleccione un monto:</h3>
-                <div class="montos">
-                    <label v-for="(monto, i) in montos" :key="i">
-                        <input type="radio" :value="monto" v-model="montoSeleccionado">
-                        {{ monto }}
-                    </label>
-                    <div class="monto-personalizado">
-                        <span>Otro:</span>
-                        <input type="number" v-model="otro" placeholder="Ingresa el monto" />
-                    </div>
-                </div>
+        <label for="mensaje">Comentario (opcional)</label>
+        <input
+          type="text"
+          id="mensaje"
+          name="mensaje"
+          placeholder="Escribe algun mensaje..."
+          v-model="comentario"
+        />
 
-                <label for="mensaje">Comentario (opcional)</label>
-                <input type="text" id="mensaje" name="mensaje" placeholder="Escribe algun mensaje..."
-                    v-model="comentario" />
+        <button id="botondonar" type="submit">Donar ahora</button>
 
+        <div class="mensaje-confirmacion" v-if="mensaje">
+          <i class="fa fa-shield-alt"></i> {{ mensaje }}
+        </div>
+      </form>
+    </main>
 
-                <button id="botondonar" type="submit">Donar ahora</button>
-
-
-
-                <div class="mensaje-confirmacion" v-if="mensaje">
-                    <i class="fa fa-shield-alt"></i> {{ mensaje }}
-                </div>
-            </form>
-        </main>
-
-        <footer>
-            <p>CIMA A.C.</p>
-        </footer>
-
-    </div>
-
-
+    <footer>
+      <p>CIMA A.C.</p>
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'DonacioneForm',
-    data() {
-        return {
-            titulo: 'CIMA A.C.',
-            preguntas: [
-                {
-                    pregunta: '¿Cómo se usa mi donativo?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿En qué proyectos impacta mi donación?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Es seguro donar en línea?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Mis datos personales y bancarios están protegidos?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Puedo obtener deducción fiscal por mi donación?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Cuánto tiempo tarda en llegar mi recibo?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Qué beneficios obtengo como donante?',
-                    respuesta: '...'
-                },
-                {
-                    pregunta: '¿Puedo contactar a alguien si tengo dudas sobre mi donación?',
-                    respuesta: '...'
-                }
-            ],
-            nombre: '',
-            correo: '',
-            telefono: '',
-            montos: ['$50', '$100', '$200', '$500'],
-            montoSeleccionado: '',
-            otro: '',
-            comentario: '',
-            mensaje: 'Tus datos están protegidos.'//Aqui lleva un icono
-        }
-    },
-    methods: {
-        enviarDonacion() {
-            // Aquí puedes manejar el envío de la donación
-            this.mensaje = '¡Gracias por tu donación!';
-        }
+  name: 'DonacioneForm'.
+  data () {
+    return {
+      titulo: "CIMA A.C.",
+      preguntas: [
+        {
+          pregunta: "¿Cómo se usa mi donativo?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿En qué proyectos impacta mi donación?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿Es seguro donar en línea?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿Mis datos personales y bancarios están protegidos?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿Puedo obtener deducción fiscal por mi donación?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿Cuánto tiempo tarda en llegar mi recibo?",
+          respuesta: "...",
+        },
+        {
+          pregunta: "¿Qué beneficios obtengo como donante?",
+          respuesta: "...",
+        },
+        {
+          pregunta:
+            "¿Puedo contactar a alguien si tengo dudas sobre mi donación?",
+          respuesta: "...",
+        },
+      ],
+      nombre: "",
+      correo: "",
+      telefono: "",
+      montos: ["$50", "$100", "$200", "$500"],
+      montoSeleccionado: "",
+      otro: "",
+      comentario: "",
+      mensaje: "Tus datos están protegidos.", //Aqui lleva un icono
+    };
+  }
+  methods: {
+    enviarDonacion() {
+      // Aquí puedes manejar el envío de la donación
+      this.mensaje = "¡Gracias por tu donación!";
     }
+  }
 }
-
 </script>
 
 <style scoped>
 body {
-  font-family: 'Poppins';
+  font-family: "Poppins";
   margin: 0;
   padding: 0;
   background: #f9f9f9;
-
 }
 header {
   background: #035928;
@@ -216,7 +231,7 @@ input::placeholder {
   border: none;
   border-radius: 6px;
   cursor: pointer;
-width: 50%;
+  width: 50%;
 }
 .mensaje-confirmacion {
   text-align: center;
@@ -235,5 +250,4 @@ footer {
   font-size: 14px;
   color: #555;
 }
-
-</style>
+</style>-->
